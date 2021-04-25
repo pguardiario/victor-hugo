740005d48190cbd0c50d5db25054f8e196debb24
P Guardiario <p.guardiario@gmail.com>
2021-04-25 10:20:13 +0100

    init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [
+    './site/layouts/partials/*.html',
+    './site/layouts/*.html',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
